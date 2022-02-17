7b3e41ee13da2a68c8ee8a4e38cddc8f1aca8820
Vinayak9769 <65537258+Vinayak9769@users.noreply.github.com>
2022-02-17 20:08:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 import requests
 app = Flask(__name__)
+from datetime import datetime
+
 
 @app.route('/')
 def hello():
@@ -34,6 +36,33 @@ def joshRank():
     json_data = response.json()
     x = json_data["data"]
     return "Josh is currently " + x["currenttierpatched"] + " with a ranked rating of " +str(x["ranking_in_tier"])
+@app.route('/josh/record', methods=['POST', 'GET'])
+def mmr():
+    y=[]
+    a=[]
+    wins = 0
+    loss = 0
+    resultString = ""
+    today = datetime.today()
+    currentDate = today.strftime("%B %d, %Y")
+    response= requests.get("https://api.henrikdev.xyz/valorant/v1/mmr-history/eu/JoshMun/Mun")
+    json_data = response.json()
+    for x in json_data["data"]:
+        splitString = x["date"].split()
+        newDate = "" +splitString[1] + " "+ splitString[2] + " "+ splitString[3]
+        if currentDate == newDate:
+            y.append(x["mmr_change_to_last_game"])
+    for n in y:
+        if n>0:
+            a.append("L")
+        else:
+            a.append("W")
+    for l in a:
+        if l == "W":
+            wins+=1
+        else:
+            loss +=1
+    return "Josh has won " + str(wins) + " games today and lost " + str(loss) + " games today. " + "Record- " + str(a)
 
 
 if __name__ == "__main__":
