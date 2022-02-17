e9508435568a20e8e631e676dbb30c6c4f1901e5
Vinayak9769 <65537258+Vinayak9769@users.noreply.github.com>
2022-02-17 13:31:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,13 @@ def getrank():
 #     for x in json_data["players"]:
 #         if x["gameName"] == "JoshMun":
 #             return "Josh is currently ranked #" + str(x["leaderboardRank"]) + " on the leaderboard with a ranked rating of " +str(x["rankedRating"] + " and " + str(x("numberOfWins")) + " wins")
-            
+@app.route('/Stout', methods=['POST', 'GET'])
+def stoutrank():
+    response = requests.get("https://api.henrikdev.xyz/valorant/v2/leaderboard/na")
+    json_data = response.json()
+    for x in json_data["players"]:
+        if x["gameName"] == "SOL Stout":
+            return "Stout is currently ranked #" + str(x["leaderboardRank"])+ " on the leaderboard with " + str(x["numberOfWins"])            
 
 if __name__ == "__main__":
     app.run(debug=True)
