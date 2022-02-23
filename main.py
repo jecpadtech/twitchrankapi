32d1e4da9d8e6c65b0c1a4d639b6460508e2d380
Vinayak9769 <65537258+Vinayak9769@users.noreply.github.com>
2022-02-23 13:12:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -182,8 +182,38 @@ def leonMaps():
 def linkaMaps():
     return maps("Pancakes","1313")
 @app.route('/stout/record', methods=['POST', 'GET'])
-def stoutRec():
-    return getRecord("SOL Stout","LUL", "Stout")
+def stoutRec():    
+    y=[]
+    a=[]
+    wins = 0
+    loss = 0
+    draw = 0
+    resultString = ""
+    today = datetime.today()
+    currentDate = today.strftime("%B %d, %Y")
+    response= requests.get("https://api.henrikdev.xyz/valorant/v1/mmr-history/na/SOL%20Stout/LUL")
+    json_data = response.json()
+    for x in json_data["data"]:
+        splitString = x["date"].split()
+        newDate = "" +splitString[1] + " "+ splitString[2] + " "+ splitString[3]
+        if currentDate == newDate:
+            y.append(x["mmr_change_to_last_game"])
+    for n in y:
+        if n>0:
+            a.append("W")
+        elif 0>n>10:
+            a.append("T")
+        else:
+            a.append("L")
+    for l in a:
+        if l == "W":
+            wins+=1
+        elif l == "L":
+            loss +=1
+        else:
+            draw +=1
+        
+    return "Stout has won " + str(wins) + " games and lost " + str(loss) + " games today. " + "Record- " + str(a) 
 
 if __name__ == "__main__":
     app.run(debug=True)
