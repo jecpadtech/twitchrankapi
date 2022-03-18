3c4b745ca9a74fe054c05aa0a5d86276d9b44c32
Vinayak9769 <65537258+Vinayak9769@users.noreply.github.com>
2022-03-18 17:10:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def getRank(name,irlname,region):
 def getRankv1(name,irlname,region):
         response = scraper.get("https://api.henrikdev.xyz/valorant/v1/leaderboard/" + region)
         json_data = response.json()
-        for x in json_data["players"]:
+        for x in json_data:
             if x["gameName"] == name:
                 return "/me " + irlname + " is currently ranked #" + str(x["leaderboardRank"])+ " on the leaderboard with " + str(x["numberOfWins"]) + " wins and a ranked rating of " + str(x["rankedRating"])
 def getRecord(name,tag,irlname):
