2a5d0cc61511f85d353039101a8252bf6eab5c23
Vinayak9769 <65537258+Vinayak9769@users.noreply.github.com>
2022-04-01 15:43:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -756,8 +756,13 @@ def spicyRank():
 def ludlb2():
     try:
         return getRankv1("CLWN Luddee","Luddee","eu")
-    except TypeError:
-        return getRank("CLWN Luddee","Luddee","eu")
+    except:
+        response = scraper.get("https://api.henrikdev.xyz/valorant/v2/leaderboard/eu")
+        json_data = response.json()
+        for x in json_data["players"]:
+            if x["gameName"] == "Guild SoMarcus":
+                return "/me " + "SoMarcus" + " is currently ranked #" + str(x["leaderboardRank"])+ " on the leaderboard with " + str(x["numberOfWins"]) + " wins and a ranked rating of " + str(x["rankedRating"])
+
 
 
 if __name__ == "__main__":
